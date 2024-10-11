f8d4f048488b7cfc681eda8699f0bd7a673aad79
Angel <aal51282@uga.edu>
2024-10-11 14:01:18 -0400

    Updated UI.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
-function App() {
+import { useState } from 'react';
+import AmountInput from './AmountInput';
 
+function App() {
+  const [amount, setAmount] = useState('100');
   return (
-    <main>test</main>
+    <main className="max-w-2xl mx-auto px-4 py-8">
+      <h1 className="uppercase text-6xl text-center font-bold bg-gradient-to-br from-purple-600 to-sky-400 bg-clip-text text-transparent from-30%">Find Cheapest BTC</h1>
+      <div className="flex justify-center mt-6">
+        <AmountInput
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)} />
+      </div>
+    </main>
   )
 }
 
---
+++ src/Input.tsx
@@ -0,0 +1,18 @@
+import { ChangeEventHandler } from "react";
+
+export type InputProps = {
+    value: string;
+    onChange: ChangeEventHandler<HTMLInputElement>;
+    className?: string;
+    placeholder?: string;
+};
+
+export default function Input(props: InputProps) {
+    return (
+        <input type="text"
+            className={"border border-white/10 bg-blue-950 p-2 " + props.className}
+            placeholder={props.placeholder}
+            value={props.value}
+            onChange={props.onChange}/>
+    );
+}
