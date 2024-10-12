739f6292444ab15e95841578d7d8caf3e98263da
Angel <aal51282@uga.edu>
2024-10-12 13:00:24 -0400

    Added to providers.

--- backend/providers.ts
+++ backend/providers.ts
@@ -34,3 +34,25 @@ export async function getOfferFromGuardarian(amount:number): Promise<string|void
         return;
     }
 }
+
+export async function getOfferFromMoonPay(amount:number): Promise<string|void> {
+    try {
+        const response = await
+        axios.get(`https://api.moonpay.com/v3/currencies/btc/buy_quote?apiKey=pk_live_R5Lf25uBfNZyKwccAZpzcxuL3ZdJ3Hc&baseCurrencyAmount=${amount}&baseCurrencyCode=usd&fixed=true&areFeesIncluded=true&quoteType=principal`,
+            return response?.data?.quoteCurrencyAmount?.toString();
+        } catch (e) {
+        console.error(e);
+        return;
+    }
+}
+
+export async function getOfferFromTransak(amount:number): Promise<string|void> {
+    try {
+        const response = await
+        axios.get(`https://api.transak.com/api/v1/pricing/public/widget/quotes?fiatCurrency=USD&cryptoCurrency=BTC&paymentMethod=credit_debit_card&isBuyOrSell=BUY&fiatAmount=${amount}&partnerApiKey=02624956-010b-4775-8e31-7b9c8b82df76&network=mainnet&quoteCountryCode=US`,
+            return response?.data?.response?.cryptoAmount?.toString();
+        } catch (e) {
+        console.error(e);
+        return;
+    }
+}
